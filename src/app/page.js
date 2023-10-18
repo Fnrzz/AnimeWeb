94ec34fe5578f028ca2c976f97057a856e48fa26
Fnrzz <faridnurrganteng123@gmail.com>
2023-10-18 08:08:58 +0700

    [push] add listpopular

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import ListPopular from "@/components/ListPopular";
 export default function Home() {
   return (
-    <div className="flex ">
-      <h1>Hello</h1>
+    <div className="flex flex-col py-5 md:px-10 px-4">
+      <h1 className="md:text-3xl text-xl font-bold mb-5">Anime Terpopuler</h1>
+      <ListPopular />
     </div>
   );
 }
--- src/utilities/Navbar/index.js
+++ src/utilities/Navbar/index.js
@@ -1,23 +1,37 @@
+"use client";
 import Styles from "./navbar.module.css";
+import { useState } from "react";
 
 const Navbar = () => {
-  const hamburger = false;
+  const [hamburger, setHamburger] = useState(false);
+
   return (
-    <nav className="shadow-md w-full">
-      <div className="md:flex justify-between items-center bg-white py-4 px-10">
-        <h1 className="text-xl font-bold">Navbar</h1>
-        <div
-          className={`${Styles["hamburger"]} ${hamburger ? "active" : ""}`}
-          onClick={() => (hamburger = !hamburger)}
-        >
-          <div className={`${Styles["line"]}`}></div>
-          <div className={`${Styles["line"]}`}></div>
-          <div className={`${Styles["line"]}`}></div>
+    <nav className="shadow-md sticky top-0 z-50 w-full">
+      <div className="md:flex justify-between items-center bg-white py-5 md:px-10 px-5">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl md:text-2xl font-bold">Anime Web</h1>
+          <div
+            className={`md:invisible ${Styles["hamburger"]} ${
+              hamburger ? Styles["active"] : ""
+            }`}
+            onClick={() => setHamburger(!hamburger)}
+          >
+            <div className={`${Styles["line"]}`}></div>
+            <div className={`${Styles["line"]}`}></div>
+            <div className={`${Styles["line"]}`}></div>
+          </div>
         </div>
-        <ul className="md:flex md:hidden  gap-4">
-          <li>Menu 1</li>
-          <li>Menu 2</li>
-          <li>Menu 3</li>
+        <ul
+          className={`md:flex gap-4 
+          ${hamburger ? "visible duration-500 transition-all" : "hidden"}`}
+        >
+          <li>
+            <input
+              type="text"
+              placeholder="Search"
+              className="rounded-lg border border-gray-300 w-full md:mt-0 mt-4 shadow px-4 py-2"
+            />
+          </li>
         </ul>
       </div>
     </nav>
