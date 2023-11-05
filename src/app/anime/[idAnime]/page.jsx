acfeaab10fc154e0e0bec5d3ef979ed580d851f0
Fnrzz <faridnurrganteng123@gmail.com>
2023-11-05 09:42:20 +0700

    [PUSH] fix bug

--- src/app/anime/[idAnime]/page.jsx
+++ src/app/anime/[idAnime]/page.jsx
@@ -10,8 +10,8 @@ const Page = async ({ params: { idAnime } }) => {
       <div className={`flex flex-col mt-10`}>
         <div className="text-xl md:text-2xl text-white">{anime.data.title}</div>
       </div>
-      <div className="flex gap-3 mt-5 flex-wrap">
-        <div className="border border-white p-2 rounded-lg">
+      <div className="flex gap-3 mt-5 flex-wrap hidden md:flex">
+        <div className="border  border-white p-2 rounded-lg">
           <div className="text-sm text-white">Type : {anime.data.type}</div>
         </div>
         <div className="border border-white p-2 rounded-lg">
@@ -31,7 +31,7 @@ const Page = async ({ params: { idAnime } }) => {
           <div className="text-sm text-white">Year : {anime.data.year}</div>
         </div>
       </div>
-      <div className="flex mt-10 flex-row gap-5 flex-wrap justify-center md:justify-start">
+      <div className="flex mt-5 md:mt-10 flex-row gap-5 flex-wrap justify-center md:justify-start">
         <Image
           src={anime.data.images.webp.image_url}
           alt={anime.data.title}
@@ -39,7 +39,30 @@ const Page = async ({ params: { idAnime } }) => {
           height={300}
           className="rounded-lg"
         />
-        <div className="text-white text-sm md:text-md lg:text-lg md:w-1/2">
+        <div className="flex gap-3 mt-5 flex-wrap block md:hidden">
+          <div className="border border-white p-2 rounded-lg">
+            <div className="text-sm text-white">Type : {anime.data.type}</div>
+          </div>
+          <div className="border border-white p-2 rounded-lg">
+            <div className="text-sm text-white">
+              Episode : {anime.data.episodes}
+            </div>
+          </div>
+          <div className="border border-white p-2 rounded-lg">
+            <div className="text-sm text-white">
+              Score : {anime.data.score} / 10
+            </div>
+          </div>
+          <div className="border border-white p-2 rounded-lg">
+            <div className="text-sm text-white">
+              Status : {anime.data.status}
+            </div>
+          </div>
+          <div className="border border-white p-2 rounded-lg">
+            <div className="text-sm text-white">Year : {anime.data.year}</div>
+          </div>
+        </div>
+        <div className="text-white text-sm md:text-sm md:w-1/2 lg:text-2xl lg:w-3/4 text-justify">
           {anime.data.synopsis}
         </div>
       </div>
